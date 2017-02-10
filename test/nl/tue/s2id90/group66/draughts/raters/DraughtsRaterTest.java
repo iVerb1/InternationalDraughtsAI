965f387cf34a54d48da5f84fc13cd7cd92e0fddb
iVerb <wimreddingius@hotmail.com>
2017-02-10 02:39:03 +0100

    Migrating codebase to GitHub

--- test/nl/tue/s2id90/group66/draughts/raters/DraughtsRaterTest.java
+++ test/nl/tue/s2id90/group66/draughts/raters/DraughtsRaterTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package nl.tue.s2id90.group66.draughts.raters;
 
 import nl.tue.s2id90.draughts.DraughtsState;
@@ -16,7 +10,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author jeroennoten
+ * @author iVerb
  */
 public class DraughtsRaterTest {
     
--- test/nl/tue/s2id90/group66/draughts/raters/PawnRaterTest.java
+++ test/nl/tue/s2id90/group66/draughts/raters/PawnRaterTest.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package nl.tue.s2id90.group66.draughts.raters;
 
 import org.junit.After;
@@ -15,7 +9,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author jeroennoten
+ * @author iVerb
  */
 public class PawnRaterTest {
     
